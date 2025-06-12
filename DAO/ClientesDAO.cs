c51a0ff00539b7a0762339d15fc6c3a79f4975fa
Felipe da Silva Gomes <2024102060028@aluno.ifro.edu.br>
2025-06-11 22:21:06 -0400

    Terceiro commit

--- DAO/ClientesDAO.cs
+++ DAO/ClientesDAO.cs
@@ -37,19 +37,76 @@ namespace AulaCRUD.DAO
 
         public void Atualizar(Clientes clientes)
         {
-            string sql = "UPDATE Clientes SET (nome_cliente = @nome_cliente, cpfCnpj = @cpfCnpj, email = @email, endereco = @endereco, telefone=@telefone WHERE id_cliente = @id_cliente);
-            
-            
+            try
+            {
+                string sql = "UPDATE Clientes SET (nome_cliente = @nome_cliente, cpfCnpj = @cpfCnpj, email = @email, endereco = @endereco, telefone=@telefone WHERE id_cliente = @id_cliente)";
+
             MySqlCommand comando = new MySqlCommand(sql, Conexao.Conectar());
-            comando.Parameters.AddWithValue("@nome_cliente", clientes.nome_cliente);
-            comando.Parameters.AddWithValue("@cpfCnpj", clientes.cpfCnpj);
-            comando.Parameters.AddWithValue("@email", clientes.email);
-            comando.Parameters.AddWithValue("@endereco", clientes.endereco);
-            comando.Parameters.AddWithValue("@telefone", clientes.telefone);
-            comando.ExecuteNonQuery();
+                comando.Parameters.AddWithValue("@nome_cliente", clientes.nome_cliente);
+                comando.Parameters.AddWithValue("@cpfCnpj", clientes.cpfCnpj);
+                comando.Parameters.AddWithValue("@email", clientes.email);
+                comando.Parameters.AddWithValue("@endereco", clientes.endereco);
+                comando.Parameters.AddWithValue("@telefone", clientes.telefone);
+                comando.ExecuteNonQuery();
 
 
-            Conexao.FecharConexao();
+                Conexao.FecharConexao();
+            }
+            catch (Exception ex)
+            { 
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Deletar(Clientes clientes)
+        {
+            try
+            {
+                string sql = "DELETE FROM Clientes WHERE id_cliente = @id_cliente)";
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao.Conectar());
+                comando.Parameters.AddWithValue("@id_cliente", clientes.id_cliente);
+                comando.ExecuteNonQuery();
+
+
+                Conexao.FecharConexao();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<Clientes> BuscarTodos()
+        {
+            List<Clientes> clientesCadastrados = new List<Clientes>();
+            try
+            {
+                string sql = "SELECT * FROM Clientes ORDER BY nome_cliente";
+                MySqlCommand comando = new MySqlCommand(sql, Conexao.Conectar());
+
+                using (MySqlDataReader dr = comando.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Clientes C = new Clientes();
+                        C.id_cliente = dr.GetInt32("id_cliente");
+                        C.nome_cliente = dr.GetString("nome_cliente");
+                        C.cpfCnpj = dr.GetString("cpfCnpj");
+                        C.email = dr.GetString("email");
+                        C.endereco = dr.GetString("endereco");
+                        C.telefone = dr.GetString("telefone");
+
+                        clientesCadastrados.Add(C);
+                    }
+                }
+                Conexao.FecharConexao();
+                return clientesCadastrados;
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception (ex.Message);
+            }
         }
     }
 }
